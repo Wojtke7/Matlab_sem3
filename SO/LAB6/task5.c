47a1987b330c6e9ba269cbf61521adde1551143a
wmarcela <wmarcela@student.agh.edu.pl>
2023-11-25 17:48:47 +0100

    lab 6 finished

---
+++ SO/LAB6/task5.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[], char *envp[])
+{
+    int env_index = 0;
+    int argv_index = 1;
+    int env_size = 0;
+
+    while (envp[env_size] != NULL) {
+        env_size++;
+    }
+
+    if(argc < 2){
+        while(envp[env_index] != NULL){
+            printf("%s\n", envp[env_index]);
+            env_index++;
+        }
+    }
+    
+
+    else {
+        while(envp[env_index] != NULL){
+            if (argv_index == argc || env_index == env_size){
+                break;
+            }
+            
+            char *variable_name = argv[argv_index];
+            char *variable_value = getenv(variable_name);
+
+            if(variable_value != NULL){
+                char result[512];  // Dostosuj rozmiar bufora do potrzeb
+                sprintf(result, "%s=%s", variable_name, variable_value);
+                printf("%s\n", result);
+                env_index = 0;
+                argv_index++;
+                continue;
+            }
+
+            else if(env_index == env_size - 1){
+                printf("Nie ma zmiennej środowiskowej o nazwie:  %s", argv[argv_index]);
+                printf("\n");
+                if (argv_index == argc - 1){
+                    break;
+                }
+                
+                else{
+                    argv_index++;
+                    env_index = 0;
+                }
+            }
+            else {
+                env_index++;
+            }
+        }
+    }
+    return 0;
+}
+
+  
